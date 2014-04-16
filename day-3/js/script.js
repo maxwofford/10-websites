560a0f875008db19e86271390e3e64ba32e63b50
MaxWofford <max.speed.wofford@gmail.com>
2014-04-16 16:56:04 -0700

    Added more demo books

--- day-3/js/script.js
+++ day-3/js/script.js
@@ -1,2 +1,8 @@
 var book = document.getElementById("bookTitle").innerHTML;
-var textFile = 'texts/' + book + '.txt';
+var textFile = './texts/' + book + '.txt';
+window.onload = function() {
+    document.getElementById("needFile").href = textFile;
+    document.getElementById("needFile").data = textFile;
+    document.getElementById("bookTitle").innerHTML = book;
+    document.getElementById("bookHeading").innerHTML = book;
+};

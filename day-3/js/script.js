fd1b94fee9a6c9480c2970f7bdebc78798b5b3ca
MaxWofford <max.speed.wofford@gmail.com>
2014-04-16 11:54:46 -0700

    Added object tag for book text on day-3

--- day-3/js/script.js
+++ day-3/js/script.js
@@ -1,14 +1,2 @@
 var book = document.getElementById("bookTitle").innerHTML;
-
-var file = 'texts/' + book + ".txt";
-
-window.onload=loadtext()
-    $.ajax({
-        async:false,
-        url: file,
-        dataType: 'text',
-        success: function(data) 
-        {
-        $('element').append(data);
-            }
-        });
+var textFile = 'texts/' + book + '.txt';
